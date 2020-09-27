57705f7b1d853ed8183cd653646465f0295bf601
StaffanPedersen <64735161+StaffanPedersen@users.noreply.github.com>
2020-09-27 22:46:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+//Declare a variable called firstVar but don't initialise it with a value.
+
+var firstVar;
+
+//Declare a variable called name and assign it your first name.
+
+var name = ("Staffan Pedersen");
+console.log(name);
+
+//Declare and initialise a variable with a number value.
+
+var number = ("10");
+console.log(number);
+
+//Create a variable called division and initialise it with a value of 20 divided by 5.
+
+var division = (20 / 5);
+console.log(division);
+
+//Write code that checks the type of the value "frog".
+
+var checkfrog = ("frog");
+if (checkfrog === "frog") {
+    console.log("frog")
+}
+else {
+    console.log("not frog")
+}
+//Declare and initialise a variable called orderHasShipped with a boolean value.
+
+var orderHasShipped = true;
+
+
+//Create an if statement that checks if orderHasShipped is true. If it is true, console log the string value "true". If not, console log the string value "false".
+
+var orderHasShipped = ("true");
+
+if (orderHasShipped === "true") {
+    console.log("True")
+}
+else {
+    console.log ("false")
+}
+
+//Create a for loop that counts from 0 to 9. Console log the value of the counter variable inside the loop.
+
+for(var i= 0; i <= 9; i++) {
+    console.log(i);
+}
